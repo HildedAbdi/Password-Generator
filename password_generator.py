d7415e89070ed64e8c3ae8b26f4499f8f50990b8
Hilded Abdi <hilded-abdi@hotmail.com>
2023-06-24 02:49:25 -0400

    Modified and added Try and Except to Password Generator

--- password_generator.py
+++ password_generator.py
@@ -3,12 +3,18 @@ import random
 characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ123456789!@#$%^&*()~"
 
 while 1:
-    Password_Len = int(input("What length would you like your password to be : "))
-    Password_count = int(input("How many passwords would you like to have generated? :"))
-    for x in range(0,Password_count):
-        Password = ""
-        for x in range (0,Password_Len):
-            Password_characters = random.choice(characters)
-            Password = Password + Password_characters
-        print("Here is your Password :", Password)
+    try:
+        Password_Len = int(input("What length would you like your password to be : "))
+        Password_count = int(input("How many passwords would you like to have generated? :"))
+    except ValueError:
+        print("Invalid Entry")
+    else:
+     for x in range(0,Password_count):
+            Password = ""
+            for x in range (0,Password_Len):
+                Password_characters = random.choice(characters)
+                Password = Password + Password_characters
+            print("Here is your Password :", Password)
+
     break
+    
